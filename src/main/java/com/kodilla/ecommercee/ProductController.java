e175671fc5fd629f8aa348c501ba5ebf63e98eca
davedohc <davedohc@aim.com>
2020-03-28 21:17:22 +0000

    Added ProductDto class, added proper returned values for some methods in ProductController
    Added changes so methods return mocked data
    
    Simplified getProductById method

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -5,6 +5,8 @@ import com.kodilla.ecommercee.exceptions.ProductNotFoundException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/v1/ecommerce")
@@ -13,12 +15,20 @@ public class ProductController {
 
     @GetMapping(value = "getProducts")
     public List<ProductDto> getProducts() {
-       return null;
+        return getMockProductList();
     }
 
     @GetMapping(value = "getProductById")
     public ProductDto getTProductById(@RequestParam Long productId) throws ProductNotFoundException {
-        return null;
+            return new ProductDto(
+                    1L,
+                    "Kurtka zimowa",
+                    "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. " +
+                            "Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. " +
+                            "Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. " +
+                            "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
+                    new BigDecimal(100),
+                    "1");
     }
 
     @PostMapping(value = "createProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -28,11 +38,58 @@ public class ProductController {
 
     @PutMapping(value = "updateProduct")
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        return null;
+        return new ProductDto(
+                1L,
+                "Kurtka zimowa",
+                "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. " +
+                        "Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. " +
+                        "Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. " +
+                        "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
+                new BigDecimal(100),
+                "1");
     }
 
     @DeleteMapping(value = "deleteProduct")
     public void deleteProduct(@RequestParam Long productId) {
 
+    }
+
+    private List<ProductDto> getMockProductList() {
+        ArrayList<ProductDto> productsList = new ArrayList<>();
+
+        ProductDto productDto1 = new ProductDto(
+                1L,
+                "Kurtka zimowa",
+                "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. " +
+                        "Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. " +
+                        "Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. " +
+                        "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
+                new BigDecimal(100),
+                "1");
+        ProductDto productDto2 = new ProductDto(
+                2L,
+                "płaszcz",
+                "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. " +
+                        "Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. " +
+                        "Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. " +
+                        "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
+                new BigDecimal(150),
+                "1");
+
+        ProductDto productDto3 = new ProductDto(
+                3L,
+                "buty",
+                "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. " +
+                        "Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. " +
+                        "Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. " +
+                        "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
+                new BigDecimal(100),
+                "4");
+
+        productsList.add(productDto1);
+        productsList.add(productDto2);
+        productsList.add(productDto3);
+
+        return productsList;
     }
 }
