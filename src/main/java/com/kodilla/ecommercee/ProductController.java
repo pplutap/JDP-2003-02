1fba64cbb88f95c8a5c29abd048d0bb808ab003b
davedohc <davedohc@aim.com>
2020-03-28 20:50:31 +0000

    First commit, task JDP200302-1, ProductController pre-implementation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.ProductDto;
+import com.kodilla.ecommercee.exceptions.ProductNotFoundException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/v1/ecommerce")
+@RestController
 public class ProductController {
+
+    @GetMapping(value = "getProducts")
+    public List<ProductDto> getProducts() {
+       return null;
+    }
+
+    @GetMapping(value = "getProductById")
+    public ProductDto getTProductById(@RequestParam Long productId) throws ProductNotFoundException {
+        return null;
+    }
+
+    @PostMapping(value = "createProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createProduct(@RequestBody ProductDto productDto) {
+
+    }
+
+    @PutMapping(value = "updateProduct")
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return null;
+    }
+
+    @DeleteMapping(value = "deleteProduct")
+    public void deleteProduct(@RequestParam Long productId) {
+
+    }
 }
