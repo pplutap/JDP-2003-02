00b36d60f7d2a89ac9ca7a4b861e4a833380d9ba
User from Noszony <nasz_email_do_github>
2020-03-30 21:54:32 +0200

    Fake data

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,11 +1,9 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.OrderDto;
-import com.kodilla.ecommercee.mapper.OrderMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -14,12 +12,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/kodilla_project")
 public class OrderController {
 
-    @Autowired
-    private OrderMapper orderMapper;
+
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrders")
     public List<OrderDto> getOrders() {
-        return orderMapper.mapToOrderDtoList(new ArrayList<>());
+        return Arrays.asList(new OrderDto(1L, 0L), new OrderDto(2L, 1L));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrder")
--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,25 +1,22 @@
 package com.kodilla.ecommercee.domain;
 
-//@NoArgsConstructor
+
 public class Order {
-   // private List<Product>products = new ArrayList<Product>();
+
     private Long orderId;
     private Long userId;
-   // private boolean isPaid;
-    //private boolean isVerified;
-   // private boolean isSubmitted;
 
 
- public Order(Long orderId, Long userId) {
-  this.orderId = orderId;
-  this.userId = userId;
- }
+    public Order(Long orderId, Long userId) {
+        this.orderId = orderId;
+        this.userId = userId;
+    }
 
- public Long getOrderId() {
-  return orderId;
- }
+    public Long getOrderId() {
+        return orderId;
+    }
 
- public Long getUserId() {
-  return userId;
- }
+    public Long getUserId() {
+        return userId;
+    }
 }
