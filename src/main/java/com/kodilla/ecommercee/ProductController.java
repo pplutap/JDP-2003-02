ac44ba73b013bc09ac9e10b619be6dc10bd12795
davedohc <davedohc@aim.com>
2020-03-30 11:00:20 +0100

    Simplified getProductById method

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/v1/ecommerce")
 @RestController
@@ -21,7 +20,6 @@ public class ProductController {
 
     @GetMapping(value = "getProductById")
     public ProductDto getTProductById(@RequestParam Long productId) throws ProductNotFoundException {
-        if (productId.equals(1L)) {
             return new ProductDto(
                     1L,
                     "Kurtka zimowa",
@@ -31,19 +29,6 @@ public class ProductController {
                             "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
                     new BigDecimal(100),
                     "1");
-        } else if (productId.equals(2L)) {
-            return new ProductDto(
-                    2L,
-                    "płaszcz",
-                    "Pellentesque tempus interdum quam ut rhoncus. Donec ullamcorper turpis dolor. " +
-                            "Donec euismod pretium eros et eleifend. Aliquam vulputate faucibus lorem non auctor. " +
-                            "Vivamus erat turpis, molestie a nisl non, scelerisque luctus enim. " +
-                            "Nunc mi mi, laoreet ac mollis nec, pharetra sit amet tortor. Vivamus a bibendum purus.",
-                    new BigDecimal(150),
-                    "1");
-        } else {
-            throw new ProductNotFoundException();
-        }
     }
 
     @PostMapping(value = "createProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
