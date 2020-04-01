ee2c18a8957f290656315fffbc7a2ada1276929f
SirVertar <mateusz.jakuszko@gmail.com>
2020-04-01 19:35:15 +0200

    Changes in line with magaleras  suggestions

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -10,17 +10,17 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class UserController {
 
     @RequestMapping(method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
-    public UserDto createUser(@RequestBody UserDto userDto) {
+    public UserDto create(@RequestBody UserDto userDto) {
         return new UserDto(1L, "Mateusz", "unblocked", 3213L);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "block/{userId}")
-    public UserDto blockUser(@PathVariable() Long userId) {
-        return new UserDto(userId, "Matusz", "blocked", 31231L);
+    @RequestMapping(method = RequestMethod.PUT, value = "/block/{id}")
+    public UserDto block(@PathVariable Long id) {
+        return new UserDto(id, "Matusz", "blocked", 31231L);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/generateKey/{userKey}")
-    public Long generateKey(@PathVariable() Long userKey) {
-        return userKey;
+    @RequestMapping(method = RequestMethod.PUT, value = "/generateKey/{id}")
+    public Long generateKey(@PathVariable Long id) {
+        return 123L;
     }
 }
