1f7ae75eb3c190e8330378984099831001ee5eab
SirVertar <mateusz.jakuszko@gmail.com>
2020-04-11 18:27:22 +0200

    Changed Group Controller, Group, GroupDto

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,10 +1,14 @@
 package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Builder
 public class GroupDto {
     private Long id;
     private String name;
