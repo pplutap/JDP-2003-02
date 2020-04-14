2342ba315361eb88224ea51fec475002cb70cc27
SirVertar <mateusz.jakuszko@gmail.com>
2020-04-14 19:56:54 +0200

    ZDP200302-212 roup_ontroller_mieniono wstrzykiwanie zależnosci przez konstruktor, usunieto niepotrzebne pola.

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.domain.GroupDto;
 import com.kodilla.ecommercee.exceptions.GroupNotFoundException;
 import com.kodilla.ecommercee.mapper.GroupMapper;
-import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.service.GroupDbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +14,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/v1/groups")
 public class GroupController {
+    private GroupMapper mapper;
+    private GroupDbService dbService;
+
     @Autowired
-    GroupMapper mapper;
-    @Autowired
-    GroupRepository repository;
-    @Autowired
-    GroupDbService dbService;
+    public GroupController(GroupMapper mapper, GroupDbService dbService) {
+        this.mapper = mapper;
+        this.dbService = dbService;
+    }
 
     @GetMapping
     public List<GroupDto> get() {
