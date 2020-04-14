c1235405f45070706d3fd88d075aeaca8c6983ee
davedohc <davedohc@aim.com>
2020-04-14 23:04:16 +0100

    Added method to OrderRepository

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findAll();
 }
