32ced2bec23421622265ce30c417ae1c2227b4f4
SirVertar <mateusz.jakuszko@gmail.com>
2020-04-14 20:04:21 +0200

    ZJP200302-2 Group_controller_zieniono wstrzykiwanie zależnosci przez konstruktor, usunieto niepotrzebne pola.

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -14,7 +14,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/v1/groups")
 public class GroupController {
-
     private GroupMapper mapper;
     private GroupDbService dbService;
 
